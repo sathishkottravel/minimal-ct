a058ff5a184e2b6d9dd8ef2a7e9fbc9679506496
sathishkottravel <sathishkottravel@gmail.com>
2018-11-20 09:48:17 +0100

    Added Sub Module Quartet.

---
+++ modules/mesherutils/ext/qtet/quartet.h
@@ -0,0 +1,17 @@
+
+#ifndef QUARTET_MAIN_H
+#define  QUARTET_MAIN_H
+#include "make_tet_mesh.h"
+#include "make_signed_distance.h"
+#include "read_obj.h"
+#include "feature.h"
+#include "trimesh.h"
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+
+int quartet_main(int argc,  char **argv);
+
+#endif // QUARTET_MAIN_H
